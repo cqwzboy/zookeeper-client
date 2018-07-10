0791407d6e9a278b1b825958352291666ff23b96
fuqinqin <904571943@qq.com>
2018-07-10 17:21:35 +0800

    reinit

--- src/main/java/com/qc/itaojin/service/ZookeeperServiceImpl.java
+++ src/main/java/com/qc/itaojin/service/ZookeeperServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.List;
  */
 @Data
 @Slf4j
-public class ZookeeperServiceImpl implements IZookeeperService {
+public class ZookeeperServiceImpl extends BaseService implements IZookeeperService {
 
     private ZooKeeper zooKeeper;
 
